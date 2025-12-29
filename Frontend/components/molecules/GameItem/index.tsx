21bc9efb752e70c3488f0bfe3d736337f5a40161
Digoox99 <rodrigo99santiago@gmail.com>
2025-12-29 17:22:49 -0300

    fix: acessibility issues

--- Frontend/components/molecules/GameItem/index.tsx
+++ Frontend/components/molecules/GameItem/index.tsx
@@ -14,37 +14,40 @@ interface GameItemProps {
     | "/assets/img/Thumbnail5.png";
   id: string;
 }
+
 const GameItem = ({ title, platform, image, id }: GameItemProps) => {
   return (
     <div className="featured-game-card position-relative">
-      <Link href={`/detail/${id}`}>
-        <a>
-          <div className="blur-sharp">
-            <img
-              className="thumbnail"
-              src={image}
-              width={205}
-              height={270}
-              alt="thumbnail-game"
-            />
-          </div>
-          <div className="cover position-absolute bottom-0 m-32">
-            <div className="d-flex flex-column h-100 justify-content-between text-decoration-none">
-              <div className="game-icon mx-auto">
-                <Image
-                  src={"/assets/icon/game.svg"}
-                  width={54}
-                  height={36}
-                  alt="ic-game"
-                />
-              </div>
-              <div>
-                <p className="fw-semibold text-white text-xl m-0">{title}</p>
-                <p className="fw-light text-white m-0">{platform}</p>
-              </div>
+      <Link
+        href={`/detail/${id}`}
+        className="text-decoration-none"
+      >
+        <div className="blur-sharp">
+          <img
+            className="thumbnail"
+            src={image}
+            width={205}
+            height={270}
+            alt="thumbnail-game"
+          />
+        </div>
+
+        <div className="cover position-absolute bottom-0 m-32">
+          <div className="d-flex flex-column h-100 justify-content-between">
+            <div className="game-icon mx-auto">
+              <Image
+                src="/assets/icon/game.svg"
+                width={54}
+                height={36}
+                alt="ic-game"
+              />
+            </div>
+            <div>
+              <p className="fw-semibold text-white text-xl m-0">{title}</p>
+              <p className="fw-light text-white m-0">{platform}</p>
             </div>
           </div>
-        </a>
+        </div>
       </Link>
     </div>
   );
