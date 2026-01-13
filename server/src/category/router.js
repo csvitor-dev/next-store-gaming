2856335e309cf4c95aeeb7e9cce866b7b9e129fe
jonaferreir4 <jonaferreira2204@gmail.com>
2026-01-13 20:26:25 -0300

    fix: delete old files

--- server/src/category/router.js
+++
@@ -1,8 +0,0 @@
-const {index} = require('./controller')
-var express = require('express');
-
-var router = express.Router();
-/* GET home page. */
-router.get('/', index);
-
-module.exports = router;
