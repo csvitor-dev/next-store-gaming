1489317914a08c720751272c56721e0d5b1a3b07
jonaferreir4 <jonaferreira2204@gmail.com>
2026-01-13 22:46:37 -0300

    image  optimized config

--- Frontend/next.config.ts
+++ Frontend/next.config.ts
@@ -1,7 +1,23 @@
-import type { NextConfig } from "next";
+import type { NextConfig } from 'next';
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  images: {
+    unoptimized: true,
+    remotePatterns: [
+      {
+        protocol: 'http',
+        hostname: 'localhost',
+        port: '3001',
+        pathname: '/images/**',
+      },
+      {
+        protocol: 'http',
+        hostname: '127.0.0.1',
+        port: '3001',
+        pathname: '/images/**',
+      },
+    ],
+  },
 };
 
 export default nextConfig;
