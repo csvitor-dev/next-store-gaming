6627f101f5b3abdba525aa8d7c89616ecf61c98e
jonaferreir4 <jonaferreira2204@gmail.com>
2026-01-13 23:02:36 -0300

    feat: api data

--- Frontend/src/app/game/[slug]/page.tsx
+++ Frontend/src/app/game/[slug]/page.tsx
@@ -1,7 +1,7 @@
 import { notFound } from "next/navigation";
 import { api } from "@/services/api";
 import { GameDetailInfo } from "@/components/organism/game-details-info";
-import { PurchaseSidebar } from "@/components/organism/purchase-sidebar"; // Reutilize o código que fiz na resposta anterior
+import { PurchaseSidebar } from "@/components/organism/purchase-sidebar";
 import { GameCardVertical } from "@/components/molecules/game-card-vertical";
 
 interface GamePageProps {
@@ -11,20 +11,26 @@ interface GamePageProps {
 }
 
 export default async function GamePage({ params }: GamePageProps) {
+
+  const resolvedParams = await params; 
+  const slug = resolvedParams.slug;
+
   let game;
   try {
-    game = await api.getGameBySlug(params.slug);
+    game = await api.getGameBySlug(slug);
   } catch (error) {
     return notFound();
   }
   
-  const recommended = (await api.getGames()).slice(0, 4);
+  const allGames = await api.getGames();
+  const recommended = allGames
+    .filter(g => g.id !== game.id)
+    .slice(0, 4);
 
   return (
-    <div className="min-h-screen bg-[#09090b] text-zinc-100 pb-20">
-      <main className="container mx-auto px-4 py-8">
+    <div className="bg-[#09090b] text-zinc-100 pb-20 xl:px-[94px]">
+      <main className="container mx-auto py-8">
         
-        {/* Layout Grid: 2 Colunas Esquerda (Info) + 1 Coluna Direita (Sidebar) */}
         <div className="grid grid-cols-1 lg:grid-cols-3 gap-10">
           
           <div className="lg:col-span-2">
@@ -36,15 +42,15 @@ export default async function GamePage({ params }: GamePageProps) {
                 <PurchaseSidebar 
                   title={game.title}
                   price={game.price}
-                  // developer={game.developer}
-                  // releaseDate={game.releaseDate}
+                  developer={game.developer}
+                  releaseDate={game.releaseDate}
                 />
              </div>
           </div>
 
         </div>
 
-=        <div className="mt-20 pt-10 border-t border-zinc-800">
+        <div className="mt-20 pt-10 border-t border-zinc-800">
           <h3 className="text-2xl font-bold text-white mb-6">Jogos Recomendados</h3>
           <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-4 gap-6">
              {recommended.map((rec) => (
