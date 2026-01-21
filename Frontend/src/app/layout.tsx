0f54d8b4947c5a70e46d1696c87a9f0872be2c6d
jonaferreir4 <jonaferreira2204@gmail.com>
2026-01-20 21:39:19 -0300

    style: wrap layout in ThemeProvider for improved theme management

--- Frontend/src/app/layout.tsx
+++ Frontend/src/app/layout.tsx
@@ -1,5 +1,6 @@
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
+import { ThemeProvider } from "../components/theme-provider"
 import { Navbar } from "@/components/organism/navbar";
 import { Footer } from "@/components/organism/footer";
 import "./globals.css";
@@ -25,17 +26,20 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="pt-br" className="dark">
-      <body
-        className={`${geistSans.variable} ${geistMono.variable} antialiased flex flex-col min-h-screen bg-background text-foreground`}
-      >
-    
-        <Navbar />
-        <div className="flex-1">
-          {children}
-        </div>
-
-        <Footer />
+    <html lang="pt-br" suppressHydrationWarning> 
+      <body className={`${geistSans.variable} ${geistMono.variable} antialiased flex flex-col min-h-screen bg-background text-foreground`}>
+        <ThemeProvider
+            attribute="class"
+            defaultTheme="dark"
+            enableSystem
+            disableTransitionOnChange
+          >
+            <Navbar />
+            <div className="flex-1">
+              {children}
+            </div>
+            <Footer />
+        </ThemeProvider>
       </body>
     </html>
   );
